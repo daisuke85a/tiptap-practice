7bccc3e7ddc390cb06683049a332270b832e3c10
daisuke <dsaito85a@gmail.com>
2020-08-30 15:47:50 +0900

    エディターのアイコンを正常表示することに成功

--- src/components/Icon/index.vue
+++ src/components/Icon/index.vue
@@ -4,13 +4,40 @@
       <use xmlns:xlink="http://www.w3.org/1999/xlink" :xlink:href="'#icon--' + name"></use>
     </svg> -->
     <svg :viewBox="symbol.viewBox"  class="icon__svg">
-      <use :xlink:href="symbol" />
+      <use :xlink:href="'#' + name" />
     </svg>
   </div>
 </template>
 
 <script>
-import bold from '../../assets/images/bold.svg';
+
+import add_col_after from '../../assets/images/add_col_after.svg';
+import bold from '../../assets/images/bold.svg';                
+import delete_col from '../../assets/images/delete_col.svg';          
+import hr from '../../assets/images/hr.svg';                  
+import mention from '../../assets/images/mention.svg';             
+import redo from '../../assets/images/redo.svg';                
+import ul from '../../assets/images/ul.svg';
+import add_col_before from '../../assets/images/add_col_before.svg';      
+import checklist from '../../assets/images/checklist.svg';           
+import delete_row from '../../assets/images/delete_row.svg';          
+import image from '../../assets/images/image.svg';               
+import ol from '../../assets/images/ol.svg';                  
+import remove from '../../assets/images/remove.svg';              
+import underline from '../../assets/images/underline.svg';
+import add_row_after from '../../assets/images/add_row_after.svg';       
+import code from '../../assets/images/code.svg';                
+import delete_table from '../../assets/images/delete_table.svg';        
+import italic from '../../assets/images/italic.svg';              
+import paragraph from '../../assets/images/paragraph.svg';           
+import strike from '../../assets/images/strike.svg';              
+import undo from '../../assets/images/undo.svg';
+import add_row_before from '../../assets/images/add_row_before.svg';      
+import combine_cells from '../../assets/images/combine_cells.svg';       
+import github from '../../assets/images/github.svg';              
+import link from '../../assets/images/link.svg';                
+import quote from '../../assets/images/quote.svg';               
+import table from '../../assets/images/table.svg';
 
 export default {
   props: {
@@ -42,9 +69,9 @@ export default {
   top: -.05rem;
   fill: currentColor;
 
-  // &.has-align-fix {
-  // 	top: -.1rem;
-  // }
+  &.has-align-fix {
+  	top: -.1rem;
+  }
 
   &__svg {
     display: inline-block;
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,32 @@
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.vue$/,
         loader: 'vue-loader',
       },
+      // this will apply to both plain `.js` files
+      // AND `<script>` blocks in `.vue` files
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
       // this will apply to both plain `.css` files
       // AND `<style>` blocks in `.vue` files
       {
         test: /\.css$/,
         use: [
-          'vue-style-loader',
+          'style-loader',
           'css-loader'
         ]
       },
       {
         test: /\.scss$/,
         use: [
-          'vue-style-loader',
+          'style-loader',
           'css-loader',
           'sass-loader'
         ]
